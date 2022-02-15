162b2ab1dfc66fa21b6dde0010c23aec6a86dd3c
Nuclear One <ajithsnair10@gmail.com>
2022-02-15 13:19:45 +0530

    test js commit

---
+++ dom.js
@@ -0,0 +1,18 @@
+//console.dir(document);
+console.log(document.title)
+document.title='pro js coder';
+console.log(document.title)
+//console.log(document.all[15])
+//document.all[15].textContent='Your Hacked';
+
+//console.log(document.getElementById('my-form'))
+let text=document.getElementById('my-form');
+console.log(text);
+
+const V=document.getElementById('header')
+//V.textContent='You are Hacked';
+//V.style.border='solid 5px black'
+V.style.borderBottom='solid 3px black';
+const H23=document.getElementById('H23');
+H23.innerHTML='<b> Hello..!! </>';
+H23.style.color='green';
